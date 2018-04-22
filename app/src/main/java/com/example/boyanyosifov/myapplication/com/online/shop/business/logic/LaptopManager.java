d8436325512c65b9c803611878856b5565e2339b
Simeon Korchev <simeonkorchev@gmail.com>
2018-04-22 15:50:18 +0300

    Enhance Managers to get All entities

--- app/src/main/java/com/example/boyanyosifov/myapplication/com/online/shop/business/logic/LaptopManager.java
+++ app/src/main/java/com/example/boyanyosifov/myapplication/com/online/shop/business/logic/LaptopManager.java
@@ -2,10 +2,21 @@ package com.example.boyanyosifov.myapplication.com.online.shop.business.logic;
 
 import android.content.Context;
 
+import com.example.boyanyosifov.myapplication.com.online.shop.repository.Database;
+import com.example.boyanyosifov.myapplication.com.online.shop.repository.Laptop;
+import com.example.boyanyosifov.myapplication.com.online.shop.repository.LaptopContentProvider;
+
+import java.util.List;
+
 public class LaptopManager implements ILaptopManager {
-    private Context context;
+    private LaptopContentProvider laptopContentProvider;
 
     public LaptopManager(Context context) {
-        this.context = context;
+        laptopContentProvider = new Database(context).getLaptopDbContentProvider();
+    }
+
+    @Override
+    public List<Laptop> getAll() {
+        return laptopContentProvider.getAll();
     }
 }
